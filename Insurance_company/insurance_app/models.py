26ed6dc7033a771d3f9cc376e8729410c0a026bc
arek001c <164615772+arek001c@users.noreply.github.com>
2024-03-26 00:29:07 +0100

    Utworzenie aplikacji insurance_app oraz stworzenie pierwszego modelu CarInsurance. Aplikacja i model - zarejestrowane.

---
+++ Insurance_company/insurance_app/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# time functions
+
+def current_year():
+    return datetime.date.today().year
+
+
+class CarInsurance(models.Model):
+
+    FUEL_TYPES = (
+        ("Gasoline", "Gasoline"),
+        ("Diesel", "Diesel"),
+        ("LPG", "LPG"),
+        ("Electric", "Electric"),
+        ("Hydrogen", "Hydrogen"),
+        ("Biodiesel", "Biodiesel")
+    )
+
+    AVERAGE_YEAR_MILEAGE = (
+        (1, "Poniżej 5 tys. km"), # kategorie przebiegu do wykorzystania w kalkulatorze
+        (2, "do 10 tys. km"),
+        (3, "do 20 tys. km"),
+        (4, "powyżej 20 tys. km")
+    )
+
+    # predefined fields
+    policy_name = models.CharField(max_length=100)
+    policy_description = models.TextField(max_length=2000)
+    valid_to = models.DateField(auto_now=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    # form fields
+    car_model = models.CharField(max_length=100)
+    production_year = models.PositiveIntegerField(default=current_year())
+    fuel_type = models.CharField(max_length=100, choices=FUEL_TYPES)
+    mileage = models.PositiveIntegerField()
+    average_year_mileage = models.PositiveSmallIntegerField(choices=AVERAGE_YEAR_MILEAGE)
+    is_rented = models.BooleanField(default=False)
+    number_of_owners = models.PositiveSmallIntegerField()
+    driver_under_26 = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Nazwa polisy: {self.policy_name}"
+
+
+
+
+
+
+
+
