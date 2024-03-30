1aeb3cdc6d808dc3b250eb01ba3367be8239ff36
arek001c <164615772+arek001c@users.noreply.github.com>
2024-03-30 12:16:19 +0100

    dodane nowe templatki - bazowa oraz index

--- Insurance_company/Insurance_company/settings.py
+++ Insurance_company/Insurance_company/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'insurance_app'
+    'insurance_app',
 ]
 
 MIDDLEWARE = [
