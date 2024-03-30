26eaeb4cae77cb6464cc11fcf7aab7cdde1e0096
Użytkownik <uzytkownik@kursGIT.pl>
2024-03-30 15:18:58 +0100

    Minor changes for the Customer model

--- Insurance_company/accounts/models.py
+++ Insurance_company/accounts/models.py
@@ -1,29 +1,26 @@
 from django.db import models
-
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from django.core.validators import RegexValidator
-from django.db import models
+
 
 User = settings.AUTH_USER_MODEL
 
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    pesel = models.CharField(max_length=11, unique=True, validators=[RegexValidator(regex='^\\d{11}$', message='PESEL must contain 11 digits')])
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    pesel = models.CharField(max_length=11, unique=True)
     address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15, blank=True)
     privacy_policy_accepted = models.BooleanField(default=False)
+    marketing_agreement = models.BooleanField(default=False)
 
-
-marketing_agreement = models.BooleanField(default=False)
+def __str__(self):
+    return f"Customer profile of {self.user.username}"
 
 
 def clean(self):
     super().clean()
-    # Sprawdzenie poprawności sumy kontrolnej PESEL
+    # Checking the correctness of the PESEL checksum
     if self.pesel:
         weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
         checksum = sum(int(self.pesel[i]) * weights[i] for i in range(10))
