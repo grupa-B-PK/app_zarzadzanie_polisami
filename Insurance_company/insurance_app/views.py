b729cccb655135247d2eed30b993e488e20a06b0
Uzytkownik <uzytkownik@kursGIT.pl>
2024-03-30 15:18:02 +0100

    Add a list view

--- Insurance_company/insurance_app/views.py
+++ Insurance_company/insurance_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
 
+from .models import CarInsurance, HouseInsurance
 
 class IndexView(TemplateView):
     template_name = 'index'
+
+class PolicyListView(TemplateView):
+    template_name = 'policy_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['car_insurances'] = CarInsurance.objects.all()
+        context['house_insurances'] = HouseInsurance.objects.all()
+        return context
