9d98abb1432f3135a9cdbe100c75b465657be130
Użytkownik <uzytkownik@kursGIT.pl>
2024-04-04 16:45:22 +0200

    Add policy_car create form, views and detail car policy

--- Insurance_company/insurance_app/urls.py
+++ Insurance_company/insurance_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from insurance_app.views import OfferListView, OfferDetailView
+from insurance_app.views import OfferListView, OfferDetailView, policy_car_create, policy_car_detail
 
 urlpatterns = [
     path('offer_list/', OfferListView.as_view(), name='offer_list'),
     path('offer_detail/', OfferDetailView.as_view(), name='offer_detail'),
+    path('offer_detail/', OfferDetailView.as_view(), name='offer_detail'),
+    path("policy_car_create/", policy_car_create),
+    path("policy_car_detail/<uuid:policy_id>", policy_car_detail, name='policy_car_detail'),
 ]
