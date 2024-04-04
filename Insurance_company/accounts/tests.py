93d5dd71d11059eebc5fbca8af9efc5c5fb95d26
Uzytkownik <uzytkownik@kursGIT.pl>
2024-04-04 03:35:49 +0200

    Made pesel tests separate

--- Insurance_company/accounts/tests.py
+++ Insurance_company/accounts/tests.py
@@ -4,22 +4,19 @@ from django.core.exceptions import ValidationError
 from accounts.models import Customer
 
 class CustomerPeselTestCase(TestCase):
-
-    def test_pesel_validation(self):
+    # Ensure that valid PESEL passes validation
+    def test_valid_pesel(self):
         valid_pesel = '44051401458'
+        self.assertIsNone(Customer(pesel=valid_pesel).clean())
+
+    # Ensure that invalid PESEL with valid checksum raises ValidationError
+    def test_invalid_pesel_with_valid_checksum(self):
         invalid_pesel_with_valid_checksum = '22222222222'
+        with self.assertRaises(ValidationError):
+            Customer(pesel=invalid_pesel_with_valid_checksum).clean()
+
+    # Ensure that PESEL with invalid checksum raises ValidationError
+    def test_invalid_pesel_with_invalid_checksum(self):
         invalid_pesel_with_invalid_checksum = '12345678900'
-
-        # Ensure that valid PESEL passes validation
-        with self.subTest(pesel=valid_pesel):
-            self.assertIsNone(Customer(pesel=valid_pesel).clean())
-
-        # Ensure that invalid PESEL with valid checksum raises ValidationError
-        with self.subTest(pesel=invalid_pesel_with_valid_checksum):
-            with self.assertRaises(ValidationError):
-                Customer(pesel=invalid_pesel_with_valid_checksum).clean()
-
-        # Ensure that PESEL with invalid checksum raises ValidationError
-        with self.subTest(pesel=invalid_pesel_with_invalid_checksum):
-            with self.assertRaises(ValidationError):
-                Customer(pesel=invalid_pesel_with_invalid_checksum).clean()
+        with self.assertRaises(ValidationError):
+            Customer(pesel=invalid_pesel_with_invalid_checksum).clean()
