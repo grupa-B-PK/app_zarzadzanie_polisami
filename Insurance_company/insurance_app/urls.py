c1c57d6d8b6e5ed6a52081faed94b45373afb7b5
Uzytkownik <uzytkownik@kursGIT.pl>
2024-04-04 20:03:35 +0200

    Fixed offers not showing

--- Insurance_company/insurance_app/urls.py
+++ Insurance_company/insurance_app/urls.py
@@ -5,6 +5,6 @@ from insurance_app.views import OfferListView, OfferDetailView, policy_car_creat
 urlpatterns = [
     path('offer_list/', OfferListView.as_view(), name='offer_list'),
     path('offer_detail/', OfferDetailView.as_view(), name='offer_detail'),
-    path("policy_car_create/", policy_car_create),
+    path("policy_car_create/", policy_car_create, name='policy_car_create'),
     path("policy_car_detail/<uuid:policy_id>", policy_car_detail, name='policy_car_detail'),
 ]
