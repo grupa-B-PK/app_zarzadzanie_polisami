a0221b53cdee9ef14984bdd4d3605ac93de87868
Uzytkownik <uzytkownik@kursGIT.pl>
2024-04-04 03:43:15 +0200

    Add pesel tests

--- Insurance_company/accounts/models.py
+++ Insurance_company/accounts/models.py
@@ -14,19 +14,17 @@ class Customer(models.Model):
     privacy_policy_accepted = models.BooleanField(default=False)
     marketing_agreement = models.BooleanField(default=False)
 
-def __str__(self):
-    return f"Customer profile of {self.user.username}"
+    def __str__(self):
+        return f"Customer profile of {self.user.username}"
 
-
-def clean(self):
-    super().clean()
-    # Checking the correctness of the PESEL checksum
-    if self.pesel:
-        weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
-        checksum = sum(int(self.pesel[i]) * weights[i] for i in range(10))
-        control_sum = checksum % 10
-        if control_sum != 0:
-            control_sum = 10 - control_sum
-        if control_sum != int(self.pesel[10]):
-            raise ValidationError('Invalid PESEL checksum')
-
+    def clean(self):
+        super().clean()
+        # Checking the correctness of the PESEL checksum
+        if self.pesel:
+            weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+            checksum = sum(int(self.pesel[i]) * weights[i] for i in range(10))
+            control_sum = checksum % 10
+            if control_sum != 0:
+                control_sum = 10 - control_sum
+            if control_sum != int(self.pesel[10]):
+                raise ValidationError('Invalid PESEL checksum')
--- Insurance_company/accounts/tests.py
+++ Insurance_company/accounts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+from accounts.models import Customer
+
+class CustomerPeselTestCase(TestCase):
+    # Ensure that valid PESEL passes validation
+    def test_valid_pesel(self):
+        valid_pesel = '44051401458'
+        self.assertIsNone(Customer(pesel=valid_pesel).clean())
+
+    # Ensure that invalid PESEL with valid checksum raises ValidationError
+    def test_invalid_pesel_with_valid_checksum(self):
+        invalid_pesel_with_valid_checksum = '22222222222'
+        with self.assertRaises(ValidationError):
+            Customer(pesel=invalid_pesel_with_valid_checksum).clean()
+
+    # Ensure that PESEL with invalid checksum raises ValidationError
+    def test_invalid_pesel_with_invalid_checksum(self):
+        invalid_pesel_with_invalid_checksum = '12345678900'
+        with self.assertRaises(ValidationError):
+            Customer(pesel=invalid_pesel_with_invalid_checksum).clean()
+
