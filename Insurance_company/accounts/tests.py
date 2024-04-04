df586bea4ac5cf9989e838801cdee6f013414b9d
Uzytkownik <uzytkownik@kursGIT.pl>
2024-04-04 02:37:39 +0200

    Add pesel tests

--- Insurance_company/accounts/tests.py
+++ Insurance_company/accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+from accounts.models import Customer
+
+class CustomerPeselTestCase(TestCase):
+
+    def test_pesel_validation(self):
+        valid_pesel = '44051401458'
+        invalid_pesel_with_valid_checksum = '22222222222'
+        invalid_pesel_with_invalid_checksum = '12345678900'
+
+        # Ensure that valid PESEL passes validation
+        with self.subTest(pesel=valid_pesel):
+            self.assertIsNone(Customer(pesel=valid_pesel).clean())
+
+        # Ensure that invalid PESEL with valid checksum passes validation
+        ''' THAT SHOULD NOT HAPPEN - WE NEED MORE SAFEGUARDS '''
+        with self.subTest(pesel=invalid_pesel_with_valid_checksum):
+            self.assertIsNone(Customer(pesel=invalid_pesel_with_valid_checksum).clean())
+
+        # Ensure that PESEL with invalid checksum raises ValidationError
+        with self.subTest(pesel=invalid_pesel_with_invalid_checksum):
+            with self.assertRaises(ValidationError):
+                Customer(pesel=invalid_pesel_with_invalid_checksum).clean()
