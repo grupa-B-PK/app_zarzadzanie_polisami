f95b8d238fe2a1145e015f3cc47e5e8a91cf4da4
Uzytkownik <uzytkownik@kursGIT.pl>
2024-04-13 16:13:08 +0200

    Added functioning price policy

--- Insurance_company/insurance_app/admin.py
+++ Insurance_company/insurance_app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from insurance_app.models import CarInsurance, HouseInsurance, CarPolicyType, HousePolicyType, CarPolicyFactors
+from insurance_app.models import CarInsurance, HouseInsurance, CarPolicyType, HousePolicyType, CarPolicyFactors, HousePolicyFactors
 
 admin.site.register(CarInsurance)
 admin.site.register(HouseInsurance)
 admin.site.register(CarPolicyType)
 admin.site.register(HousePolicyType)
 admin.site.register(CarPolicyFactors)
+admin.site.register(HousePolicyFactors)
 
 
 
