23d77ec36205a8d8460f3a9fa58a7c807964e743
Uzytkownik <uzytkownik@kursGIT.pl>
2024-04-16 23:07:24 +0200

    Added customer details

--- Insurance_company/accounts/urls.py
+++ Insurance_company/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import LogoutConfirmView, RegisterView
+from .views import LogoutConfirmView, RegisterView, CustomerDetailView
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="accounts/logged_out.html"), name="logout"),
     path("logout_confirm/", LogoutConfirmView.as_view(), name="logout_confirm"),
     path("register/", RegisterView.as_view(), name="register"),
+    path("customer_detail/<int:pk>", CustomerDetailView.as_view(), name="customer_detail"),
 ]
