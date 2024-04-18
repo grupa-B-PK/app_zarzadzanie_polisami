829d1f7841973e263777516c8af42ca6ae1f2a58
Uzytkownik <uzytkownik@kursGIT.pl>
2024-04-18 19:49:16 +0200

    Add Customer update

--- Insurance_company/accounts/views.py
+++ Insurance_company/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View, DetailView
 from django.http import Http404
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from accounts.models import Customer
@@ -63,3 +64,42 @@ class CustomerDetailView(View):
         }
 
         return render(request, 'accounts/customer_detail.html', context)
+
+
+class CustomerUpdateView(View):
+    def get(self, request, pk):
+        customer = get_object_or_404(Customer, pk=pk)
+
+        if not request.user.is_authenticated:
+            return render(request, "404.html")
+        elif request.user.customer != customer:
+            return render(request, "404.html")
+
+        form = CustomerForm(instance=customer)
+        context = {'form': form, 'customer': customer}
+
+        return render(request, 'accounts/customer_update.html', context)
+
+    def post(self, request, pk):
+        customer = get_object_or_404(Customer, pk=pk)
+        form = CustomerForm(request.POST, instance=customer)
+
+        if not request.user.is_authenticated:
+            return render(request, "404.html")
+        elif request.user.customer != customer:
+            return render(request, "404.html")
+
+        if form.is_valid():
+            new_password1 = form.cleaned_data.get('new_password1')
+            new_password2 = form.cleaned_data.get('new_password2')
+
+            if new_password1 and new_password1 == new_password2:
+                customer.user.set_password(new_password1)
+                customer.user.save()
+
+            form.save()
+            messages.success(request, "Pomyślnie wprowadzono nowe dane.")
+            return redirect('customer_detail', pk=customer.pk)
+
+        context = {'form': form, 'customer': customer}
+        return render(request, 'accounts/customer_update.html', context)
